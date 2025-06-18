26bad1bafa11b8005a9aca84e1c04a454ff5f14e
goodhamgupta <shubhamg2208@live.com>
2025-06-19 01:44:34 +0530

    Fix postgrex types in prod

--- config/runtime.exs
+++ config/runtime.exs
@@ -48,7 +48,8 @@ if config_env() == :prod do
     pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
     socket_options: maybe_ipv6,
     ssl: true,
-    ssl_opts: [verify: :verify_none]
+    ssl_opts: [verify: :verify_none],
+    types: JumpstartAi.PostgrexTypes
 
   # The secret key base is used to sign/encrypt cookies and other secrets.
   # A default value is used in config/dev.exs and config/test.exs but you
